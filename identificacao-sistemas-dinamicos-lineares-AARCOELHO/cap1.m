1737fa819d012babb7cc0d4a15569a0949666187
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-03-10 23:42:12 -0300

    Update cap1.m adding ex1.2. But it has error

--- identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap1.m
+++ identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap1.m
@@ -20,3 +20,17 @@ figure;
 printsys(num, den1, 's');
 step(num, den1);
 
+%{
+Ex1.2-Estimador parametro planta
+equacao;
+%}
+y(1)=0; u(1)=0; soma1=0; soma2=0;
+for i=2:30
+    u(i)=1; y(i)=0.9*y(i-1)+u(i-1);
+end
+for k=1:30
+    soma1=soma1+y(k)*(y(k+1)-u(k));
+    soma2=soma2+y(k)^2;
+end
+t=1:30
+plot(t,teta(t));
