62549a7d75cfbccb20c3b240429631568442b513
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-03-18 23:28:03 -0300

    Update cap3.m with example 3.2

--- identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap3.m
+++ identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap3.m
@@ -10,3 +10,14 @@ polos=roots(denz)
 dstep(numz, denz)                   % resposta
 title('resposta a a sequencia unitaria');
 ylabel('saida'), xlabel('amostras)
+
+% Exemplo 3.2
+% Geracao ruido branco
+% media nula e variancia unitaria
+N=100;
+x = rand(N,1) % media 0.5 e variancia 0.083
+y = 2*x-1; % media nula e variancia 0.33
+z = sqrt(3)*y; % media nula e variancia 1
+mean(z);var(z);
+plot(z);
+ylabel('e(t)'), xlabel('amostras')
