8537470a6f78adb2e84c528e0d28b983283ebf3e
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-04-06 23:52:25 -0300

    add example 4.6

--- identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap4/cap4.m
+++ identificacao-sistemas-dinamicos-lineares-AARCOELHO/cap4/cap4.m
@@ -1,5 +1,6 @@
 %% Exemplo 4.3
 % Estimacao de modelo 2a ordem por metodo Mollenkamp
+clear; clc;
 
 t1 = input(' valor de t1(15%): ');
 t2 = input(' valor de t2(45%): ');
@@ -32,3 +33,35 @@ else
     disp('atraso de transporte')
     teta
 end
+
+%% Exemplo 4.6
+% Estimacao por metodo da resposta impulsiva do exemplo 4.6
+clear; clc;
+
+na = 3; nb = 4;
+h(1) = 7.157039; h(2) = 9.491077; h(3) = 8.563889;
+h(4) = 5.930506; h(5) = 2.845972; h(6) = 0.144611;
+
+for i = 1:na
+    for j = 1:na
+        ha(i,j) = h(i+j-1);
+    end
+    haux1(i) = -h(i+3);
+end
+ha1 = inv(ha);
+ah = ha1*haux1';
+a = [ah(3) ah(2) ah(1)]
+
+for j = 1:nb
+    for i = 1:nb
+        if j>i
+            hb(j,i) = a(j-i);
+        elseif j == i;
+            hb(j,i) = 1;
+        else
+            hb(j,i) = 0;
+        end
+    end
+end
+haux2 = [0 h(1) h(2) h(3)];
+b = (hb*haux2')'
